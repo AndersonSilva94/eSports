887b3f671c651b95248b7f367226e810937f1097
Anderson Silva <andssilva.23@gmail.com>
2022-09-14 00:29:18 -0400

    feat: :rocket: adicionando o título

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -5,6 +5,9 @@ function App() {
   return (
     <div className='max-w-[1344] mx-auto flex flex-col items-center my-20'>
       <img src={logo} />
+      <h1 className='text-6xl text-white font-black mt-20'>
+        Seu <span className='bg-nlw-gradient text-transparent bg-clip-text'>duo</span> está aqui.
+      </h1>
     </div>
   )
 }
