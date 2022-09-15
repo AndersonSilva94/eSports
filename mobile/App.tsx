41e5b4e4bd6fa8e1c8c188e4b5410e6d4e5f7196
Anderson Silva <andssilva.23@gmail.com>
2022-09-14 21:48:36 -0400

    feat: :rocket: criando o componente de background

--- mobile/App.tsx
+++ mobile/App.tsx
@@ -1,38 +1,10 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View, TouchableOpacity } from 'react-native';
-
-interface ButtonProps {
-  title: string;
-}
-
-function Button(props: ButtonProps) {
-  return (
-    <TouchableOpacity>
-      <Text>
-        {props.title}
-      </Text>
-    </TouchableOpacity>
-  );
-}
+import { Background } from './src/components/Background';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Hello React Native!</Text>
-
-      <Button title='Send 1' />
-      <Button title='Send 2' />
-      <Button title='Hello World!' />
-      <StatusBar style="auto" />
-    </View>
+    <Background>
+      
+    </Background>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  }
-});
