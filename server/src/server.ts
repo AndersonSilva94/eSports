21faba954f34d52d6acb4ef6991b4be9f21cd006
Anderson Silva <andssilva.23@gmail.com>
2022-09-16 01:25:06 -0400

    feat: :rocket: criando a rota que lista anúncios por game

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,10 +1,23 @@
 import express from 'express';
+import { PrismaClient } from "@prisma/client"
 
 const app = express();
+const prisma = new PrismaClient({
+  log: ['query'] // forma de dar um console.log no prisma
+});
 
 // listar games com número de anúncios
-app.get('/games', (request, response) => {
-  return response.json([])
+app.get('/games', async (request, response) => {
+  const games = await prisma.game.findMany({
+    include: {
+      _count: {
+        select: {
+          ads: true,
+        }
+      }
+    }
+  });
+  return response.json(games);
 })
 
 // criar anúncios
@@ -13,15 +26,33 @@ app.post('/ads', (request, response) => {
 })
 
 // listar anúncios por game
-app.get('/games/:id/ads', (request, response) => {
-  // const gameId = request.params.id
+app.get('/games/:id/ads', async (request, response) => {
+  const gameId = request.params.id
 
-  return response.json([
-    { id: 1, name: 'Anúncio 1' },
-    { id: 2, name: 'Anúncio 2' },
-    { id: 3, name: 'Anúncio 3' },
-    { id: 4, name: 'Anúncio 4' },
-  ])
+  const ads = await prisma.ad.findMany({
+    select: {
+      id: true,
+      name: true,
+      weekDays: true,
+      useVoiceChannel: true,
+      yearsPlaying: true,
+      hoursStart: true,
+      hoursEnd: true,
+    },
+    where: {
+      gameId,
+    },
+    orderBy: {
+      createdAt: 'desc',
+    }
+  })
+
+  return response.json(ads.map(ad => {
+    return {
+      ...ad,
+      weekDays: ad.weekDays.split(',')
+    }
+  }))
 })
 
 // buscar discord pelo ID do anúncio
