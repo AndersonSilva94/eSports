8b49b3bb12797628b8f9087464190d3055cb3bb4
Anderson Silva <andssilva.23@gmail.com>
2022-09-17 03:50:08 -0400

    feat: :rocket: chamando a api para buscar os dados de games

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,12 +1,31 @@
+import { useState, useEffect } from "react";
 import "./styles/main.css";
 import logo from "./assets/logo-nlw-esports.svg";
 import { GameBanner } from "./components/GameBanner";
-import { CreateAdBanner } from './components/CreateAdBanner';
+import { CreateAdBanner } from "./components/CreateAdBanner";
 
-function App() {
+interface Game {
+  id: string;
+  title: string;
+  bannerUrl: string;
+  _count: {
+    ads: number;
+  }
+}
+
+const App = () => {
+  const [games, setGames] = useState<Game[]>([]);
+
+  useEffect(() => {
+    fetch("http://localhost:3333/games")
+      .then((res) => res.json())
+      .then((data) => setGames(data));
+  }, []);
+
   return (
     <div className="max-w-[1344] mx-auto flex flex-col items-center my-20">
       <img src={logo} />
+
       <h1 className="text-6xl text-white font-black mt-20">
         Seu{" "}
         <span className="bg-nlw-gradient text-transparent bg-clip-text">
@@ -14,12 +33,21 @@ function App() {
         </span>{" "}
         está aqui.
       </h1>
+
       <div className="grid grid-cols-6 gap-6 mt-16">
-        <GameBanner bannerUrl='/game-1.png' title='League of Legends' adsCount={2} />
+        {games.map((game) => (
+          <GameBanner
+            key={game.id}
+            bannerUrl={game.bannerUrl}
+            title={game.title}
+            adsCount={game._count.ads}
+          />
+        ))}
       </div>
+
       <CreateAdBanner />
     </div>
   );
-}
+};
 
 export default App;
