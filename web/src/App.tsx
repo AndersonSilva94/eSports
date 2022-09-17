00b9e4f577c2ed879fdc6357aeccc314892fbfe7
Anderson Silva <andssilva.23@gmail.com>
2022-09-17 04:47:08 -0400

    feat: :rocket: instalando biblioteca de modal e implementando na aplicação

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,4 +1,5 @@
 import { useState, useEffect } from "react";
+import * as Dialog from "@radix-ui/react-dialog";
 import "./styles/main.css";
 import logo from "./assets/logo-nlw-esports.svg";
 import { GameBanner } from "./components/GameBanner";
@@ -10,7 +11,7 @@ interface Game {
   bannerUrl: string;
   _count: {
     ads: number;
-  }
+  };
 }
 
 const App = () => {
@@ -45,7 +46,18 @@ const App = () => {
         ))}
       </div>
 
-      <CreateAdBanner />
+      <Dialog.Root>
+        <CreateAdBanner />
+
+        <Dialog.Portal>
+          <Dialog.Overlay className="bg-black opacity-70 inset-0 fixed" />
+
+          <Dialog.Content className="fixed bg-[#2A2634] py-8 px-10 text-white top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 rounded-lg w-[480px] shadow-lg shadow-black">
+            <Dialog.Title>Publique um anúncio</Dialog.Title>
+            <Dialog.Content>Qualquer coisa</Dialog.Content>
+          </Dialog.Content>
+        </Dialog.Portal>
+      </Dialog.Root>
     </div>
   );
 };
